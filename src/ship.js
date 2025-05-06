f4b82de07cba4d412bb40a82f689582ebcb82aaf
emremark <mark.radicc@gmail.com>
2025-05-06 22:22:00 +0200

    Add ship.js module
    ShipModule created to store the IIFE with the Ship
    class and methods.

---
+++ src/ship.js
@@ -0,0 +1,11 @@
+export const ShipModule = (function () {
+    
+    class Ship {
+        constructor(shipLen) {
+            this.shipLen = shipLen;
+            this.hitCount = 0;
+            this.sunk = false
+        }
+    }
+})();
+
