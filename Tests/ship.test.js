be6e13e6e4c003f481c3ffb1490eb11e1abbf489
emremark <mark.radicc@gmail.com>
2025-05-07 23:52:36 +0200

    Add test for createShip

--- Tests/ship.test.js
+++ Tests/ship.test.js
@@ -6,3 +6,12 @@ test("hit() function to increase hitCount on ship by 1", () =>{
     aShip.hit();
     expect(aShip.hitCount).toBe(1);
 })
+
+test("createShip(len) to return a ship object", () => {
+    const aShip = ShipModule.createShip(3);
+    expect(aShip).toMatchObject({
+        shipLen: 3,
+        hitCount: 0,
+        sunk: false
+    })
+})
